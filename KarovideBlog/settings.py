838877590e41d4cce7fb685631d1bb99a7f407e5
mhmdrzrasi <mhmdrzrasi@gmail.com>
2023-08-12 12:28:24 +0330

    edit base address of statics and templates files

--- KarovideBlog/settings.py
+++ KarovideBlog/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'KarovideBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,11 +118,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
