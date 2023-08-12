1e095a665710af62f12ad1cfcd6c3e58b861604b
mhmdrzrasi <mhmdrzrasi@gmail.com>
2023-08-12 11:08:45 +0330

    basic settings for future

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Journalist, Editor
+
+admin.site.register(Journalist)
+admin.site.register(Editor)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Journalist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='journalist')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Editor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='editor')
+
+    def __str__(self):
+        return self.user.get_full_name()
