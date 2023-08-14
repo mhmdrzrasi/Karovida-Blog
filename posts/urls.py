ca10fb1f0c8645647b3c7ee72ff9ac1aeb1a3d7f
mhmdrzrasi <mhmdrzrasi@gmail.com>
2023-08-14 10:38:54 +0330

    create view and url for display posts page

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+app_name = 'posts'
+urlpatterns = [
+    path('', views.PostsView.as_view(), name='posts'),
+    path('posts/<int:post_id>/', views.PostView.as_view(), name='post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,78 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.http import HttpRequest
 
-from .models import Post, RawPost, Category, Tag
-from .forms import PostForm, RawPostForm, CategoryForm, TagForm
-
-
-# Admin, Journalist, Editor
-class RawPostDetail(View):
-
-    def get(self, request: HttpRequest):
-        pass
-
-    def post(self, request: HttpRequest):
-        pass
-
-    def put(self, request: HttpRequest):
-        pass
-
-    # except Editor
-    def delete(self, request: HttpRequest):
-        pass
+from .models import Post, Category, Tag
 
 
 # Admin - Journalist
-class PostDetail(View):
+class PostsView(View):
 
     def get(self, request: HttpRequest):
-        pass
-
-    def post(self, request: HttpRequest):
-        pass
-
-    def put(self, request: HttpRequest):
-        pass
-
-    def delete(self, request: HttpRequest):
-        pass
+        posts = Post.objects.filter(status='C')
+        return render(request, 'posts/posts.html', {'posts': posts})
 
 
-#  Admin, Journalist, Editor
-class CategoryDetail(View):
+class PostView(View):
 
-    def get(self, request: HttpRequest):
-        pass
+    def get(self, request: HttpRequest, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        return render(request, '', {post})  # todo
 
-    def post(self, request: HttpRequest):
-        pass
-
-    def put(self, request: HttpRequest):
-        pass
-
-    def delete(self, request: HttpRequest):
-        pass
+# #  Admin, Journalist, Editor
+# class CategoryDetail(View):
+#
+#     def get(self, request: HttpRequest):
+#         pass
+#
+#     def post(self, request: HttpRequest):
+#         pass
+#
+#     def put(self, request: HttpRequest):
+#         pass
+#
+#     def delete(self, request: HttpRequest):
+#         pass
 
 
 # Admin, Journalist, Editor
-class TagDetail(View):
-
-    def get(self, request: HttpRequest):
-        pass
-
-    def post(self, request: HttpRequest):
-        pass
-
-    def put(self, request: HttpRequest):
-        pass
-
-    def delete(self, request: HttpRequest):
-        pass
+# class TagDetail(View):
+#
+#     def get(self, request: HttpRequest):
+#         pass
+#
+#     def post(self, request: HttpRequest):
+#         pass
+#
+#     def put(self, request: HttpRequest):
+#         pass
+#
+#     def delete(self, request: HttpRequest):
+#         pass
 
 
 # for show to client
-class PostView(View):
-
-    def get(self, request: HttpRequest):
-        pass
