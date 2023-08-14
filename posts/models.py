6b083515a758aa18745638ebcd2c94ee23c7cede
mhmdrzrasi <mhmdrzrasi@gmail.com>
2023-08-14 10:31:09 +0330

    create Post Category Tag models

--- posts/models.py
+++ posts/models.py
@@ -2,54 +2,70 @@ from django.db import models
 
 from accounts.models import User
 from .constants import STATUS_CHOICE
+from persiantools.jdatetime import JalaliDate
+from datetime import datetime
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=256)
-    content = models.TextField()
-    image = models.ImageField()
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
+
+    title = models.CharField(max_length=256, help_text='حداکثر 256 کاراکتر', verbose_name='عنوان')
+    short_description = models.TextField(max_length=512, help_text='حداکثر 512 کاراکتر', verbose_name='توضیح کوتاه')
+    content = models.TextField(help_text='محتوا اصلی پست که به صورت HTML وارد شود', verbose_name='محتوا پست')
+    image = models.ImageField(upload_to='uploads/', null=True, blank=True, verbose_name='عکس')
+
     slug = models.SlugField()
-    publish_date = models.DateField()
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='post')
-    tags = models.ManyToManyField('Tag', related_name='post')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    # todo: whenever journalist accept that
+    publish_date = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ انتشار')
 
-    status = models.CharField(max_length=1, choices=STATUS_CHOICE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author', verbose_name='نویسنده')
+    acceptor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='post_acceptor',
+                                 verbose_name='تایید کننده')
 
-    def __str__(self):
-        return self.title
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='post', verbose_name='دسته بندی')
+    tags = models.ManyToManyField('Tag', related_name='post', blank=True, verbose_name='برچسب ها')
 
+    post_id = models.CharField(max_length=128, unique=True, help_text='به عنوان id داخل فایل HTML استفاده می شود')
 
-class RawPost(models.Model):
-    title = models.CharField(max_length=256)
-    content = models.TextField()
-    image = models.ImageField()
-    slug = models.SlugField()
-    publish_date = models.DateField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICE, verbose_name='وضعیت')
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='raw_post')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='raw_post')
-    tags = models.ManyToManyField('Tag', related_name='raw_post')
-
-    status = models.CharField(max_length=1, choices=STATUS_CHOICE)
-
+    @property
+    def jalali_date_time(self):
+        ad_date = self.publish_date
+        if not ad_date:
+            return JalaliDate.to_jalali(datetime.now())
+        #     If you want to display the 'time' at the same time, you can use JalaliDateTime
+        return JalaliDate.to_jalali(datetime(ad_date.year, ad_date.month, ad_date.day))
 
     def __str__(self):
         return self.title
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=1024)
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    name = models.CharField(max_length=256, verbose_name='اسم')
+    description = models.CharField(max_length=1024, null=True, blank=True, help_text='حداکثر 1024 کاراکتر',
+                                   verbose_name='توضیحات')
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=1024)
+    class Meta:
+        verbose_name = 'برچسب'
+        verbose_name_plural = 'برچسب ها'
+
+    name = models.CharField(max_length=256, verbose_name='اسم')
+    description = models.CharField(max_length=1024, null=True, blank=True, help_text='حداکثر 1024 کاراکتر',
+                                   verbose_name='توضیحات')
 
     def __str__(self):
         return self.name
