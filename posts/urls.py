cea68bee4232f136c9afa8dd98fe211063d953c5
mhmdrzrasi <mhmdrzrasi@gmail.com>
2023-08-19 12:24:24 +0330

    create demo form to see all of the posts

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.PostsView.as_view(), name='posts'),
-    path('posts/<int:post_id>/', views.PostView.as_view(), name='post')
+    path('posts/', views.PostsView.as_view(), name='posts'),
+    path('posts/<int:post_id>/', views.PostView.as_view(), name='post'),
+    path('posts/demo/', views.PostsDemoView.as_view(), name='posts-demo'),
+    path('posts/<int:post_id>/demo/', views.PostDemoView.as_view(), name='post-demo'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,11 @@ from django.views import View
 from django.http import HttpRequest
 
 from .models import Post, Category, Tag
+from accounts.models import User
+
+
+def is_admin(user: User):
+    return user.is_staff
 
 
 # Admin - Journalist
@@ -10,14 +15,30 @@ class PostsView(View):
 
     def get(self, request: HttpRequest):
         posts = Post.objects.filter(status='C')
-        return render(request, 'posts/posts.html', {'posts': posts})
+        return render(request, 'posts/posts.html', {'posts': posts, 'demo': ''})
 
 
 class PostView(View):
 
     def get(self, request: HttpRequest, post_id):
+        post = get_object_or_404(Post, id=post_id, status='C')
+        return render(request, 'posts/post.html', {'post': post})
+
+
+class PostsDemoView(View):
+    def get(self, request: HttpRequest):
+        # if is_admin(request.user):
+        posts = Post.objects.all()
+        return render(request, 'posts/posts.html', {'posts': posts, 'demo': 'demo'})
+        # return render(request, '404.html')
+
+
+class PostDemoView(View):
+    def get(self, request: HttpRequest, post_id):
+        # if is_admin(request.user):
         post = get_object_or_404(Post, id=post_id)
         return render(request, 'posts/post.html', {'post': post})
+        # return render(request, '404.html')
 
 # #  Admin, Journalist, Editor
 # class CategoryDetail(View):
